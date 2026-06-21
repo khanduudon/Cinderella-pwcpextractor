ce2131eb905b58ee45c2e5a3260fd5db1ecc296f
msbizrise-arch <msbizrise@gmail.com>
2026-06-21 10:29:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(os.environ.get("API_ID", "38498066"))
 API_HASH = os.environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 THUMB_URL = "https://graph.org/file/5e7dc66913185f41c81ce-78485dba7e5ae7fc8e.jpg"
-THUMB_PATH = "document_thumb.jpg"
+THUMB_PATH = "document_thumb_v2.jpg"
 
 OWNER = int(os.environ.get("OWNER", "8446475678"))
 
