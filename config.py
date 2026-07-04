b4f4e030a8dd8c0ddc1a6b9c69c167a0ffc38833
coderpagluu <chavdasujal08@gmail.com>
2026-07-04 22:39:11 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,22 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "38498066"))
 API_HASH = os.environ.get("API_HASH", "c9696114751feacdeb1b4487f5839a1a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8874847847:AAHx7iDZxD_i8zehAUKzLWm0w42dYdBviYM")
 THUMB_URL = "https://ibb.co/tpTLJ5wv"
 THUMB_PATH = "document_thumb_v2.jpg"
 
-OWNER = int(os.environ.get("OWNER", "8909902924"))
+OWNER = int(os.environ.get("OWNER", "8727689182"))
 
 AUTH_USER = os.environ.get(
     "AUTH_USERS",
-    "6660248311,6446087354,8480660521,8680968748,8446475678,7988815969,8429278856,7920113547,8723278238,8715662594,8838086114").split(',')
+    "8727689182,6446087354,8480660521,8680968748,8446475678,7988815969,8429278856,7920113547,8723278238,8715662594,8838086114").split(',')
 AUTH_USERS = [int(uid) for uid in AUTH_USER if uid.strip()]
 if OWNER not in AUTH_USERS:
     AUTH_USERS.append(OWNER)
 
 # ── Logging Channel ────────────────────────────────────────────────────────
 # Format: -100123456789 (negative group chat ID)
-LOG_CHANNEL = (os.environ.get("LOG_CHANNEL", "-1004370124638"))
+LOG_CHANNEL = (os.environ.get("LOG_CHANNEL", "-1004301546823"))
 
 # ── Video Player Configuration ─────────────────────────────────────────────
 HEROKU_VIDEO_URL = os.environ.get(
